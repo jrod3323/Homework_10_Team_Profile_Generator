fa9f5b570299c23e85e36b9b5a5ca93f9b9c5f2d
Jared Ackley <jrod3323@gmail.com>
2020-12-16 18:35:27 -0600

    add intern class

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+let employees = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
@@ -33,3 +33,5 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+// questions ---> no more employees ---> render(employees);
